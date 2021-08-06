22c850b6d90bcffc02abf496922e2d92c4666abb
Caio Bartholomeu <caio.sadu@gmail.com>
2021-08-05 22:28:47 -0300

    First Commit

---
+++ ProjectOne/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  ProjectOne
+//
+//  Created by Caio Bartholomeu on 05/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Group{
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+
+            Section{
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectOne/ProjectOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOneApp.swift
+//  ProjectOne
+//
+//  Created by Caio Bartholomeu on 05/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
